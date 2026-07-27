bb3a21cc67de3a8569df2597a1d91ac115ab9c40
roymss <roymartinsilaban199@gmail.com>
2026-07-27 11:35:28 +0700

    Delete all file testing

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
-$kernel->bootstrap();
-$res = Illuminate\Support\Facades\DB::select('SHOW COLUMNS FROM users');
-foreach($res as $r) {
-    echo $r->Field . ' - ' . $r->Type . ' - ' . $r->Key . ' - ' . $r->Extra . "\n";
-}
